aa800f4f6b3b0e59f791757bfc5940981b880d31
Yevhenii <organistiy@gmail.com>
2022-12-15 21:20:10 +0100

    smth

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,33 @@
-import pizzas from '../assets/pizza.json';
+import React from 'react';
+
 import Header from './Header';
 import Categories from './Categories';
 import Sort from './Sort';
 import PizzaBlock from './PizzaBlock';
+// import Spinner from './Spinner';
 
 import '../scss/app.scss';
 
 function App() {
+  const [items, setItems] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch("https://639b4f9ed5141501975219ce.mockapi.io/pizzas")
+      .then((res) => {
+        return res.json()
+      })
+      .then(json => {
+        setItems(json)
+      })
+  }, [])
+
+
+
 
   return (
 
     <div className="wrapper">
+      {/* <Spinner /> */}
       <Header />
       <div className="content">
         <div className="container">
@@ -21,7 +38,7 @@ function App() {
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
             {
-              pizzas.map((obj) => (
+              items.map((obj) => (
                 <PizzaBlock key={obj.id} {...obj} />
               ))
             }
---
+++ src/components/Spinner.jsx
@@ -0,0 +1,9 @@
+import SpinnerSVG from '../assets/img/spinner.svg';
+
+const Spinner = () => {
+
+    // return (
+    // )
+}
+
+export default Spinner;
