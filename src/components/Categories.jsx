016494dd86a657581a2b419fbfffb0c279808798
Yevhenii <organistiy@gmail.com>
2022-12-20 20:25:28 +0100

    update func category/sort

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,23 +1,20 @@
 import React from "react";
 
-const Categories = () => {
+const Categories = ({ value, onChangeCategory }) => {
 
     const categories = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые'];
-    const [activeIndex, setActiveIndex] = React.useState(0);
 
-    const onClickCategory = (id) => {
-        setActiveIndex(id);
-    }
+
 
     return (
         <div className="categories">
             <ul>
-                {categories.map((item, id) => {
+                {categories.map((categoryName, id) => {
                     return (
                         <li
-                            className={activeIndex === id ? "active" : ''}
-                            onClick={() => onClickCategory(id)}
-                            key={id}>{item}
+                            className={value === id ? "active" : ''}
+                            onClick={() => onChangeCategory(id)}
+                            key={id}>{categoryName}
                         </li>
                     )
                 })}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,9 +8,16 @@ import Skeleton from '../components/PizzaBlock/Skeleton';
 const Home = () => {
     const [items, setItems] = React.useState([]);
     const [isLoading, setIsLoading] = React.useState(true);
+    const [categoryId, setCategoryId] = React.useState(0);
+    const [sortType, setSortType] = React.useState({
+        name: 'популярности',
+        sortProperty: 'rating'
+    });
+
 
     React.useEffect(() => {
-        fetch("https://639b4f9ed5141501975219ce.mockapi.io/pizzas")
+        setIsLoading(true);
+        fetch(`https://639b4f9ed5141501975219ce.mockapi.io/pizzas?${categoryId > 0 ? `category=${categoryId}` : ''}&sortBy=${sortType.sortProperty}&order=desc`)
             .then((res) => {
                 return res.json()
             })
@@ -19,13 +26,13 @@ const Home = () => {
                 setIsLoading(false);
             })
         window.scrollTo(0, 0);
-    }, [])
+    }, [categoryId, sortType])
 
     return (
         <div className="container">
             <div className="content__top">
-                <Categories />
-                <Sort />
+                <Categories value={categoryId} onChangeCategory={(id) => setCategoryId(id)} />
+                <Sort value={sortType} onChangeSort={(id) => setSortType(id)} />
             </div>
             <h2 className="content__title">Все пиццы</h2>
 
