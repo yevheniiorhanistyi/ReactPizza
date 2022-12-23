8366251b3ffcd13603998f2dc9ee49615fa33cf0
Yevhenii <organistiy@gmail.com>
2022-12-23 01:50:21 +0000

    changed pagineted-items component

---
+++ src/components/PaginatedItems/index.jsx
@@ -0,0 +1,45 @@
+import ReactPaginate from 'react-paginate';
+import React from 'react';
+
+import PizzaBlock from '../PizzaBlock';
+import Skeleton from '../PizzaBlock/Skeleton';
+
+import styles from './PaginatedItems.module.scss';
+
+const PaginatedItems = ({ pizzas, isLoading}) => {
+    const [itemOffset, setItemOffset] = React.useState(0);
+
+    const skeletons = [...new Array(4)].map((_, index) => <Skeleton key={index} />);
+    const endOffset = itemOffset + 4;
+    const currentItems = pizzas.slice(itemOffset, endOffset);
+    const pageCount = Math.ceil(pizzas.length / 4);
+    
+  
+    const handlePageClick = (event) => {
+      const newOffset = (event.selected * 4) % pizzas.length;
+
+      setItemOffset(newOffset);
+    };
+
+    return (
+        <>
+            <div className="content__items">
+                {isLoading ? skeletons : currentItems.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
+            </div>
+            <ReactPaginate
+                className={styles.root}
+                breakLabel="..."
+                previousLabel="<"
+                nextLabel=">"
+                onPageChange={handlePageClick}
+                pageRangeDisplayed={4}
+                pageCount={pageCount}
+                renderOnZeroPageCount={null}
+            />
+        </>
+        
+    )
+}
+
+
+export default PaginatedItems;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,16 +4,13 @@ import { SearchContext } from '../components/App';
 import Categories from '../components/Categories';
 import Sort from '../components/Sort';
 import Search from "../components/Search";
-import PizzaBlock from '../components/PizzaBlock';
-import Skeleton from '../components/PizzaBlock/Skeleton';
-import Pagination from "../components/Pagination";
+import PaginatedItems from "../components/PaginatedItems";
 
 const Home = () => {
     const { searchValue } = React.useContext(SearchContext);
     const [items, setItems] = React.useState([]);
     const [isLoading, setIsLoading] = React.useState(true);
     const [categoryId, setCategoryId] = React.useState(0);
-    const [currentPage, setCurrentPage] = React.useState(1);
     const [sortType, setSortType] = React.useState({
         name: 'Popularność',
         sortProperty: 'rating'
@@ -26,7 +23,7 @@ const Home = () => {
         const order = sortType.sortProperty.includes('-') ? 'asc' : 'desc';
         const category = categoryId > 0 ? `category=${categoryId}` : '';
 
-        fetch(`https://639b4f9ed5141501975219ce.mockapi.io/pizzas?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}`)
+        fetch(`https://639b4f9ed5141501975219ce.mockapi.io/pizzas?${category}&sortBy=${sortBy}&order=${order}`)
             .then((res) => {
                 return res.json()
             })
@@ -35,16 +32,15 @@ const Home = () => {
                 setIsLoading(false);
             })
         window.scrollTo(0, 0);
-    }, [categoryId, sortType, searchValue, currentPage])
+
+    }, [categoryId, sortType, searchValue])
 
     const pizzas = items.filter((obj) => {
         if (obj.title.toLowerCase().includes(searchValue.toLowerCase())) {
             return true;
         }
         return false;
-    }).map((obj) => <PizzaBlock key={obj.id} {...obj} />);
-
-    const skeletons = [...new Array(4)].map((_, index) => <Skeleton key={index} />);
+    });
 
     return (
         <div className="container">
@@ -53,10 +49,7 @@ const Home = () => {
                 <Sort value={sortType} onChangeSort={(id) => setSortType(id)} />
             </div>
             <Search />
-            <div className="content__items">
-                {isLoading ? skeletons : pizzas}
-            </div>
-            <Pagination onChangePage={(number) => setCurrentPage(number)} />
+            <PaginatedItems pizzas={pizzas} isLoading={isLoading}/>
         </div>
     )
 }
