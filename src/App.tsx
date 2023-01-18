8796d43174c9e8765d7705e4f5acf6021b2fc02a
Yevhenii <organistiy@gmail.com>
2023-01-18 17:49:52 +0100

    was added react-lazy/suspense

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,11 @@ import { Routes, Route } from 'react-router';
 
 import MainLayout from './layouts/mainLayout';
 import Home from './pages/Home';
-import NotFound from './pages/NotFound';
 
 import './scss/app.scss';
 
-const Cart = React.lazy(() => import('./pages/Cart'));
+const Cart = React.lazy(() => import(/* webpackChunkName: "Cart" */'./pages/Cart'));
+const NotFound = React.lazy(() => import(/* webpackChunkName: "NotFound" */'./pages/NotFound'));
 
 const App: React.FC = () => {
 
@@ -15,12 +15,20 @@ const App: React.FC = () => {
     <Routes>
       <Route path='/' element={<MainLayout />} >
         <Route path='' element={<Home />} />
-        <Route path='/cart' element={
-          <Suspense fallback={<div>Loading...</div>}>
-            <Cart />
-          </Suspense>
-        } />
-        <Route path='*' element={<NotFound />} />
+        <Route
+          path='/cart'
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <Cart />
+            </Suspense>
+          } />
+        <Route
+          path='*'
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <NotFound />
+            </Suspense>
+          } />
       </Route>
     </Routes>
   );
